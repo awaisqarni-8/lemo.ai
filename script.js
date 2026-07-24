8fd16528888e4463e4a4cd29698799e858cf5b01
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-24 21:42:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector("button").addEventListener("click", function () {
   alert("Welcome to Lemo.ai!");
 });
+window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
