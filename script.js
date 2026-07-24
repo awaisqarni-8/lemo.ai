c0b5b524673b1ba8a7e8304af0a7a88cb46554f4
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-24 22:19:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeBtn.innerHTML = "☀️";
+    } else {
+        themeBtn.innerHTML = "🌙";
+    }
+});
