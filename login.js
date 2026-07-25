0de8ba4026e3e360bd9a0a4d3663f8e153dfbd45
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-25 21:12:30 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,43 +1,28 @@
 import { auth } from "./firebase.js";
-
 import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "index.html";
+  }
+});
 
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-e.preventDefault();
+  const email = form.querySelector('input[type="email"]').value;
+  const password = form.querySelector('input[type="password"]').value;
 
-const email = form.querySelector('input[type="email"]').value;
-const password = form.querySelector('input[type="password"]').value;
-
-try{
-
-if(window.location.pathname.includes("signup")){
-
-await createUserWithEmailAndPassword(auth,email,password);
-
-alert("Account Created Successfully!");
-
-window.location.href="login.html";
-
-}else{
-
-await signInWithEmailAndPassword(auth,email,password);
-
-alert("Login Successful!");
-
-window.location.href="index.html";
-
-}
-
-}catch(error){
-
-alert(error.message);
-
-}
-
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login Successful!");
+    window.location.href = "index.html";
+  } catch (error) {
+    alert(error.message);
+  }
 });
