17ab7645f504c2caf74486042e6ddad6a5c7aeb3
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-25 18:09:51 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,1 +1,30 @@
-g
+export default async function handler(req, res) {
+  const API_KEY = process.env.GEMINI_API_KEY;
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { message } = req.body;
+
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [{ text: message }]
+          }
+        ]
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
