fbc35de8d06a3a744e2d7df15af126421c02b006
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-25 14:18:24 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBZNwEnzoaMiHlC9k62ni2sn3WrR4uNd0",
+  authDomain: "lemoai.firebaseapp.com",
+  projectId: "lemoai",
+  storageBucket: "lemoai.firebasestorage.app",
+  messagingSenderId: "779033931600",
+  appId: "1:779033931600:web:5069edb3f04ca4881a4655"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
