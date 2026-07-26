569100a441931c3f5ce55ab3283b4e47218d3807
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-26 17:27:18 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const { message } = req.body;
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`,
     {
       method: "POST",
       headers: {
