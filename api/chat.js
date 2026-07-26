a8e6676f6d95e051851731ac1fe6a9cf6a0ca663
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-26 13:54:22 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -27,5 +27,8 @@ export default async function handler(req, res) {
   const data = await response.json();
 console.log(JSON.stringify(data, null, 2));
 
-  res.status(200).json(data);
+  res.status(200).json({
+  apiKeyExists: !!API_KEY,
+  data
+});
 }
