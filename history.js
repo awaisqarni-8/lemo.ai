a08a37b2d7b7fa29fe03b7abb7489e1ca2f7701a
awaisqarni-8 <awaisqarnismaq4@gmail.com>
2026-07-26 20:23:08 +0500

    Update history.js

--- history.js
+++ history.js
@@ -1,4 +1,5 @@
 import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 import { collection, getDocs, query, orderBy, where } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
 const historyList = document.getElementById("historyList");
